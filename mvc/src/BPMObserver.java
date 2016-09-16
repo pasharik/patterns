6dff7f2caeef95382cc25093e3a6ae35b17e63b2
Pavel Pozdeev <pavel.pozdeev@texunatech.com>
2016-09-16 17:45:15 +0100

    MVC

--- mvc/src/BPMObserver.java
+++ mvc/src/BPMObserver.java
@@ -1,6 +1,3 @@
-/**
- * User: Pavel Pozdeev, Texuna Technologies
- * Date: 9/9/2016
- */
 public interface BPMObserver {
+	void updateBPM();
 }
---
+++ mvc/src/HeartTestDrive.java
@@ -0,0 +1,6 @@
+public class HeartTestDrive {
+    public static void main(String[] args) {
+        HeartModel heartModel = new HeartModel();
+        ControllerInterface model = new HeartController(heartModel);
+    }
+}
